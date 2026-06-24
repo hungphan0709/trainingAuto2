004913e7463f514bbce45f79637ef1ffff7a30a3
Hungphan <hungpq@onepay.vn>
2026-06-24 19:58:22 +0700

    thaydoilan1
    thay đổi lần 1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto("https://google.com/")
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright123/);
+  await expect(page).toHaveTitle(/hungpqdeptrai/);
 });
 
 test('get started link', async ({ page }) => {
