9f8af6f1dae9e0f54051660bc48d4e487fa325da
Hungphan <hungpq@onepay.vn>
2026-06-24 19:28:49 +0700

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto("https://google.com/")
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright123/);
+  await expect(page).toHaveTitle(/Playwright1_thay_doi_main/);
 });
 
 test('get started link', async ({ page }) => {
