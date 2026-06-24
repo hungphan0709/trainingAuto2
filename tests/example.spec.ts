3897e90d997cf02c8380bddd9b924a22f9ef69c5
Hungphan <hungpq@onepay.vn>
2026-06-24 19:23:09 +0700

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto("https://google.com/")
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright123/);
+  await expect(page).toHaveTitle(/hungpq_ver1.1.1/);
 });
 
 test('get started link', async ({ page }) => {
