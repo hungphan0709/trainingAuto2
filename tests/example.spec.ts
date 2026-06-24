bd3a03e6ff6487e906332cc403c22ae1563a4614
Hungphan <hungpq@onepay.vn>
2026-06-24 19:35:42 +0700

    Revert "Update example.spec.ts"
    This reverts commit 4dc75bf111bcdddda0e8a58e88f4e328450672ef.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,10 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
 <<<<<<< HEAD
-  await expect(page).toHaveTitle(/hungpq_ver1.1.1.1/);
-
+  await expect(page).toHaveTitle(/hungpq_ver1.1.1/);
+=======
+  await expect(page).toHaveTitle(/Playwright1_thay_doi_main/);
+>>>>>>> main
 });
 
 test('get started link', async ({ page }) => {
